1089ddc24c851cc6dcee718c4a68445ee3f54962
Pabil <pabil.adhikari10@gmail.com>
2023-08-16 19:24:42 -0700

    Updated styling before adding links

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,19 +6,28 @@ export default function NavBar(){
         <nav className="navbar">
             <h1>Your Spotify Analytics</h1>
                 <SearchBar />
-                <ul className="navbar--items">
-                    <li className="navbar--titles">General</li>
-                    <li><Link to="/">Home</Link ></li>
-                    <li><Link to="/stats">Stats</Link ></li>
-                        
-                    <li className="navbar--titles">Favourites</li>
-                    <li><Link to="/songs">Favourite Songs</Link ></li>
-                    <li><Link to="/artists">Favourite Artists</Link ></li>
-                    <li><Link to="/albums">Favourite Albums</Link ></li>
+                <div className="navbar--items">
+                <ul className="navbar--list"> 
+                    <div className="navbar--general">
+                        <li>General</li>
+                        <li><Link to="/">Home</Link ></li>
+                        <li><Link to="/stats">Stats</Link ></li>
+                    </div>
 
-                    <li className="navbar--titles">Personalization</li>
-                    <li><Link to="/settings">Settings</Link ></li>
+                    <div className="navbar--favs">
+                        <li>Favourites</li>
+                        <li><Link to="/songs">Favourite Songs</Link ></li>
+                        <li><Link to="/artists">Favourite Artists</Link ></li>
+                        <li><Link to="/albums">Favourite Albums</Link ></li>
+                    </div> 
+
+                    <div className="navbar--personal">
+                        <li>Personalization</li>
+                        <li><Link to="/settings">Settings</Link ></li>
+                    </div>
+
                 </ul>
+                </div>
         </nav>
     );
 };
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,4 +1,18 @@
+import {useState} from "react"
 
 export default function SearchBar(){
-
+    
+    /* const [input, setInput] = useState("") */
+    
+    return (
+        <div className="searchbar">
+            <input 
+                type="text"
+                placeholder="Search here"
+                className="searchbar--input"
+                /* onChange={handlechange}
+                value={searchInput} */
+            />
+        </div>
+    );
 }
