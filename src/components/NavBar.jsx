a32053541cd6f57ad189b31169ccea6d7cd543f2
Pabil <pabil.adhikari10@gmail.com>
2023-08-16 21:12:14 -0700

    Finished the final design of navbar and page layouts

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,31 +3,26 @@ import SearchBar from "./SearchBar"
 
 export default function NavBar(){
     return (
-        <nav className="navbar">
-            <h1>Your Spotify Analytics</h1>
+        <div className="navbar">
+            <ul className="navbar--list"> 
+            <div className="navbar--items">
+
+                <h1>Your Spotify Analytics</h1>
                 <SearchBar />
-                <div className="navbar--items">
-                <ul className="navbar--list"> 
-                    <div className="navbar--general">
-                        <li>General</li>
-                        <li><Link to="/">Home</Link ></li>
-                        <li><Link to="/stats">Stats</Link ></li>
-                    </div>
+                <li className="navbar--title"><h1>General</h1></li>  
+                <li><Link to="/">Home</Link ></li>
+                <li><Link to="/stats">Stats</Link ></li>
+    
+                <li className="navbar--title"><h1>Favourites</h1></li>  
+                <li><Link to="/songs">Favourite Songs</Link ></li>
+                <li><Link to="/artists">Favourite Artists</Link ></li>
+                <li><Link to="/albums">Favourite Albums</Link ></li>
+                 
 
-                    <div className="navbar--favs">
-                        <li>Favourites</li>
-                        <li><Link to="/songs">Favourite Songs</Link ></li>
-                        <li><Link to="/artists">Favourite Artists</Link ></li>
-                        <li><Link to="/albums">Favourite Albums</Link ></li>
-                    </div> 
-
-                    <div className="navbar--personal">
-                        <li>Personalization</li>
-                        <li><Link to="/settings">Settings</Link ></li>
-                    </div>
-
-                </ul>
-                </div>
-        </nav>
+                <li className="navbar--title"><h1>Personalization</h1></li>
+                <li><Link to="/settings">Settings</Link ></li>
+            </div> 
+            </ul>
+        </div>
     );
 };
