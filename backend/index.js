ba3e86f3aae9da47ea5e3e50c22032c7b382dc9f
Pabil <pabil.adhikari10@gmail.com>
2023-09-14 18:27:18 -0700

    Created the backend express server and added routing and Oauth for spotify login

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+console.log(process.env.CLIENT_ID);
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get('/', (req, res) => {
+    res.send("Hello World")
+});
+
+app.get('/login', (req, res) => {
+    res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`)
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
